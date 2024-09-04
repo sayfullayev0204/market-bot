99e4d712c83ef3feded26d344418ae55d2b91fef
G'ayrat Sayfullayev <136235060+sayfullayev0204@users.noreply.github.com>
2024-09-04 08:51:16 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -139,8 +139,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-        # "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 5,
