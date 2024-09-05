ecfb3514355631359a569416eb05ab50cd6d9c48
G'ayrat Sayfullayev <136235060+sayfullayev0204@users.noreply.github.com>
2024-09-05 17:29:29 +0500

    Update views.py

--- api/views.py
+++ api/views.py
@@ -186,7 +186,7 @@ def order_detail(request, pk):
 def send_confirmation_message(telegram_id, order_id, confirmed):
     import requests
 
-    bot_token = '6804578580:AAF_urAMl6LAy20iaFehqAxsOEFd4K1P8Bs'
+    bot_token = '6804578580:AAFgkIvNyRzzLRhaouWCzyBRJ-87jUk6OAs'
     if confirmed:
         text = f"Ваш заказ с идентификатором {order_id} подтвержден!\n Доставка через 29 дней."
     else:
